01775029139f623e3aadc44e5c666b44c90a6da7
Brian Alexander Amezquita Parada <bap00258@hotmail.com>
2023-04-07 10:18:40 -0500

    responsive

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -4,7 +4,7 @@ const ProductList = ({productos}) => {
     return (
         
     <div className="container">
-        <div className="row row-cols-4 row-cols-md-4 row-cols-lg-4 row-cols-xl-10 g-4">
+        <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5 g-4">
             {productos.map((producto) => (
                 <div className="col" key={producto.id}>
                     <Card producto={producto}/>
--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -6,22 +6,15 @@ export const Card = ({producto}) => {
         
         <Link to={`${producto.id}`} className='text-decoration-none'>
 
-
-
-                <div className="card target text-black ">
-                        <img src={producto.Image} width={20} height={30} className="card-img-top" alt="..."/>
-                        <div class="card-body  ">
-                            <h5 className="card-title text-center text-black ">{producto.title}</h5>
-                            <p className="card-text text-center"> $ {producto.price}</p>
-                        </div>
+            <div className="card target text-black">
+                <img src={producto.Image} className="card-img-top img-fluid" alt="..."/>
+                <div className="card-body">
+                    <h5 className="card-title text-center text-black">{producto.title}</h5>
+                    <p className="card-text text-center">$ {producto.price}</p>
                 </div>
-
-
+            </div>
 
         </Link>
-        
-
-
     
     )
 }
